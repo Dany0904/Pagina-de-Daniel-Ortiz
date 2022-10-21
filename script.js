2bc589f5b96908249d9996c06757c47a2ee53f9e
Daniel Ortiz <albertodanielortiz04@gmail.com>
2022-10-20 19:41:38 -0500

    Se integra por completo info de API

--- script.js
+++ script.js
@@ -6,35 +6,36 @@ xhr.open("GET", "https://reqres.in/api/users", true);
 xhr.onload = function(){
     const datos = xhr.response;
     const datosObject = JSON.parse(datos);
-    //document.getElementById('nombre').innerHTML = datosObject.data[0].first_name;
-    /* for(dato of datosObject.data){
-    
-    } */
-    //console.log(datosObject.data[0].first_name);
-    let divContainCard = document.createElement('div');
-    divContainCard.classList.add('containCard');
 
-    let divImg =  document.createElement('div');
-    divImg.classList.add('imagenproducto');
-    divImg.classList.add('imagenUno');
+    let divContainCard, divImg, card, pNombre, pEmail, textNombre, textEmail, avatarImg;
 
-    let card = document.createElement('div');
-    card.classList.add('card1');
+    for(let i = 0; i < 6; i++) {
+        divContainCard = document.createElement('div');
+        divContainCard.classList.add('containCard');
 
-    let pNombre = document.createElement('p');
-    let pEmail = document.createElement('p');
+        divImg =  document.createElement('div');
+        divImg.classList.add('imagenProducto');
 
-    contenedorPrincipal.appendChild(divContainCard);
-    divContainCard.appendChild(divImg);
-    divContainCard.appendChild(card);
-    card.appendChild(pNombre);
-    card.appendChild(pEmail);
+        card = document.createElement('div');
+        card.classList.add('card1');
 
-    var textNombre = document.createTextNode(datosObject.data[0].first_name + ' ' +datosObject.data[0].last_name)
-    pNombre.append(textNombre);
-    var textEmail = document.createTextNode(datosObject.data[0].email)
-    pEmail.append(textEmail);
-    
-    console.log(contenedorPrincipal);
+        pNombre = document.createElement('p');
+        pEmail = document.createElement('p');
+
+        contenedorPrincipal.appendChild(divContainCard);
+        divContainCard.appendChild(divImg);
+        divContainCard.appendChild(card);
+        card.appendChild(pNombre);
+        card.appendChild(pEmail);
+
+        textNombre = document.createTextNode(datosObject.data[i].first_name + ' ' +datosObject.data[i].last_name)
+        pNombre.append(textNombre);
+        textEmail = document.createTextNode(datosObject.data[i].email);
+        pEmail.append(textEmail);
+
+        avatarImg = datosObject.data[i].avatar;
+        divImg.style.backgroundImage = 'url('+avatarImg+')';
+        pNombre.style.fontWeight = "bold";
+    }
 };
 xhr.send(); 
